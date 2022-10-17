a3a4c0fbb8f58f8392c6f305409bddb8523b98d3
Lawrence Henry <97918440+lehenry24@users.noreply.github.com>
2022-10-17 17:16:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('what is up');
+
+const btn = document.querySelector('#button');
+
+btn.addEventListener('click', () =>{
+    console.log('button was cliced');
+    alert('You clicked the button');
+});
